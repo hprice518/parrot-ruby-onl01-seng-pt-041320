4466fa0a0c4453185bba770680f562a109523f3f
Harrison Price <hprice518@gmail.com>
2020-04-16 01:07:01 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,15 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot
-  puts "Squawk!"
-end
 
-parrot
-
-def parrot(bird)
-  puts "Squawk!"
-end
-
-parrot("Squawk!")
 
